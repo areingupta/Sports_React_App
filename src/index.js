c264ec6fcd71029318a6deb4b846f3f51af2c978
Arein Gupta <76914288+areingupta@users.noreply.github.com>
2023-02-24 20:18:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-  <Auth0Provider domain='dev-oa3p5wa3azdbsrfz.us.auth0.com' clientId='6uomjeM9peS5IUh3xOIserpYlAawcIkG' authorizationParams={{
+  <Auth0Provider domain='' clientId='' authorizationParams={{
     redirect_uri: window.location.origin
   }}>
     <App />
